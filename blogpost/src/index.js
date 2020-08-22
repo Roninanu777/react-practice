7e9a47c0286150d9c53cdeb3344026a34ce7ac4f
Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-08-22 12:21:51 +0530

    initial commit

---
+++ blogpost/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import './index.css';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+);
